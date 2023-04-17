d820195b561e65edbb4d008023a6e6239f6a7a7a
Fábio Martins <fabiomtv@outlook.com.br>
2023-04-17 11:35:33 -0300

    erro apos clicar no botao alterar

--- WebForms_Projeto/AdicionarCadastro.aspx
+++ WebForms_Projeto/AdicionarCadastro.aspx
@@ -8,8 +8,32 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+        <asp:Button ID="btn_Adicionar" runat="server" Text="Adicionar" OnClick="btn_Adicionar_Click" />
+        <asp:Button ID="btn_Apagar" runat="server" Text="Apagar" OnClick="btn_Apagar_Click" />
+        <p>
+            <asp:Label ID="lbl_Id" runat="server" Text="ID:"></asp:Label>
+            <asp:TextBox ID="txt_Id" runat="server" Height="16px"></asp:TextBox>
+        </p>
+        <asp:Label ID="lbl_Nome" runat="server" Text="Nome"></asp:Label>
+        <asp:TextBox ID="txt_Nome" runat="server" Height="16px"></asp:TextBox>
+        <p>
+            <asp:Label ID="lbl_Ddd" runat="server" Text="DDD:"></asp:Label>
+            <asp:TextBox ID="txt_Ddd" runat="server" Height="16px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="lbl_Tel" runat="server" Text="Telefone:"></asp:Label>
+            <asp:TextBox ID="txt_Tel" runat="server" Height="16px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="lbl_TipoTel" runat="server" Text="Tipo Tel"></asp:Label>
+            <asp:TextBox ID="txt_Tipo" runat="server" Height="16px"></asp:TextBox>
+        </p>
+        <asp:DropDownList ID="DropDownList1_TipoTel" runat="server">
+            <asp:ListItem>Movel</asp:ListItem>
+            <asp:ListItem>Fixo</asp:ListItem>
+            <asp:ListItem>Comercial</asp:ListItem>
+            <asp:ListItem>Recado</asp:ListItem>
+        </asp:DropDownList>
     </form>
 </body>
 </html>
--- WebForms_Projeto/AdicionarCadastro.aspx.cs
+++ WebForms_Projeto/AdicionarCadastro.aspx.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +12,64 @@ namespace WebForms_Projeto
 {
     public partial class AdicionarCadastro : System.Web.UI.Page
     {
+        OracleConnection con = new OracleConnection();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btn_Adicionar_Click(object sender, EventArgs e)
+        {
+            String sql = "INSERT INTO FABIOTELEFONE(ID, NOME, DDD, TELEFONE, TIPO) " +
+                "VALUES(:ID, :NOME, :DDD, :TELEFONE, :TIPO)";
+            this.AUD(sql, 0);
+        }
+
+        protected void btn_Apagar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AUD(String sql_stmt, int state)
+        {
+            String msg = "";
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = sql_stmt;
+            cmd.CommandType = CommandType.Text;
+
+            switch (state)
+            {
+                case 0:
+                    msg = "Linha inseida com sucesso!";
+                    cmd.Parameters.Add("ID", OracleDbType.Int32, 6).Value = Int32.Parse(txt_Id.Text);
+                    cmd.Parameters.Add("NOME", OracleDbType.Varchar2, 25).Value = txt_Nome.Text;
+                    cmd.Parameters.Add("DDD", OracleDbType.Varchar2, 25).Value = txt_Ddd.Text;
+                    cmd.Parameters.Add("TELEFONE", OracleDbType.Varchar2, 25).Value = txt_Tel.Text;
+                    cmd.Parameters.Add("TIPO", OracleDbType.Varchar2, 10).Value = txt_Tipo.Text;
+
+                    break;
+                case 1:
+                    //msg = "Row Updated Successfully!";
+                    //cmd.Parameters.Add("LAST_NAME", OracleDbType.Varchar2, 25).Value = last_name_txtbx.Text;
+                    //cmd.Parameters.Add("EMAIL", OracleDbType.Varchar2, 25).Value = email_txtbx.Text;
+                    //cmd.Parameters.Add("HIRE_DATE", OracleDbType.Date, 7).Value = hire_date_picker.SelectedDate;
+
+                    //cmd.Parameters.Add("EMPLOYEE_ID", OracleDbType.Int32, 6).Value = Int32.Parse(employee_id_txtbx.Text);
+
+                    break;
+                case 2:
+                    //msg = "Row Deleted Successfully!";
+
+                    //cmd.Parameters.Add("EMPLOYEE_ID", OracleDbType.Int32, 6).Value = Int32.Parse(employee_id_txtbx.Text);
+
+                    break;
+            }
+            try
+            {
+                int n = cmd.ExecuteNonQuery();
+ 
+            }
+            catch (Exception expe) { }
         }
     }
 }
